fbdcdbd47f5fbd530f5de9648d9690ce827f794f
Mahanthy <chandu.mahanthy@capgemini.com>
2018-07-14 15:49:36 +0530

    chandu ETL nob

---
+++ DB_Migate_File_0.1/DB_Migate_File/DB_Migate_File_run.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;db_migate_file_0_1.jar;' ge_deployment.db_migate_file_0_1.DB_Migate_File  --context=Default %* 
